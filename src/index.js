2877892689b04348e04a9df82ca32bcebcda8463
Veronika <veronika13072004@gmail.com>
2025-08-20 12:08:53 +0300

    Added solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,28 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  let result = '';
+
+  for (let i = 0; i < expr.length; i += 10) {
+    const block = expr.slice(i, i + 10);
+
+    if (block === '**********') {
+      result += ' ';
+    } else {
+      let morse = '';
+      for (let j = 0; j < block.length; j += 2) {
+        const letter = block.slice(j, j + 2);
+
+        if (letter === '10') {
+          morse += '.';
+        } else if (letter === '11') {
+          morse += '-';
+        }
+      }
+      result += MORSE_TABLE[morse];
+    }
+  }
+  return result;
 };
